fdf0d36d518aea79e18c5f5dab063aedf4e70304
pierre.tsiakkaros <pierre.tsiakkaros@gmail.com>
2024-06-27 12:02:06 +0200

    :bug: fix build error due to missing prop type def

--- app/confirm-email/page.tsx
+++ app/confirm-email/page.tsx
@@ -1,9 +1,15 @@
-const ConfirmEmailPage = ({ searchParams }) => {
+const ConfirmEmailPage = ({
+  searchParams,
+}: {
+  searchParams: { email: string };
+}) => {
   return (
     <div className="flex justify-center items-center h-screen">
       <h1 className="block scroll-m-20 text-xl  tracking-tight lg:text-2xl">
         Thank you ! You just created an account with the email address:
-        <p className="w-full text-center text-indigo-400 font-bold">{searchParams.email} </p>
+        <p className="w-full text-center text-indigo-400 font-bold">
+          {searchParams.email}{" "}
+        </p>
       </h1>
     </div>
   );
